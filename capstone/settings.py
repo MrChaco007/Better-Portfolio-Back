4ce4c1e0e708f899224f36c6487684867936ae88
Alexander Chaconas <alexander.chaconas@gmail.com>
2021-03-04 17:23:56 -0500

    updated cors headers

--- capstone/settings.py
+++ capstone/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'portfolio_back.apps.PortfolioBackConfig',
-    'corsheaders'
+    'django.corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    'django.corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'capstone.urls'
